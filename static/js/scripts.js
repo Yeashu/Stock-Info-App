e696c83648e43e0d6075b9029fe1f8899e400d92
Yeashu <yeashusemwal@gmail.com>
2025-01-06 12:06:22 +0530

    Enhance styling and improve stock info display with additional formatting

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -8,45 +8,47 @@ document.getElementById('stock-form').addEventListener('submit', async function(
     if (response.ok) {
         const data = await response.json();
         stock_info_div.innerHTML = `
-            <h2>${ticker} (${data['longName']})</h2>
+            <h2>${data['longName']} (${ticker.toUpperCase()})</h2>
             <p>${data['longBusinessSummary']}</p>
-            <h3>Price: $${data['currentPrice']}</h3>
-            <h3>Market Cap: $${data['marketCap']}</h3>
-            <h3>Dividend Yield: ${data['dividendYield']}%</h3>
-            <h3>EPS: $${data['trailingEps']}</h3>
-            <h3>PE Ratio: ${data['trailingPE']}</h3>
-            <h3>52 Week High: $${data['fiftyTwoWeekHigh']}</h3>
-            <h3>52 Week Low: $${data['fiftyTwoWeekLow']}</h3>
+            <h3>Price: $${data['currentPrice'].toFixed(2)}</h3>
+            <h3>Market Cap: $${data['marketCap'].toFixed(2)}</h3>
+            <h3>Dividend Yield: ${data['dividendYield'].toFixed(2)}%</h3>
+            <h3>EPS: $${data['trailingEps'].toFixed(2)}</h3>
+            <h3>PE Ratio: ${data['trailingPE'].toFixed(2)}</h3>
+            <h3>52 Week High: $${data['fiftyTwoWeekHigh'].toFixed(2)}</h3>
+            <h3>52 Week Low: $${data['fiftyTwoWeekLow'].toFixed(2)}</h3>
             <table>
-                <tr>
-                    <th>Indicator</th>
-                    <th>Value</th>
-                </tr>
-                <tr>
-                    <td>Beta</td>
-                    <td>${data['beta']}</td>
-                </tr>
-                <tr>
-                    <td>Debt to Equity</td>
-                    <td>${data['debtToEquity']}%</td>
-                </tr>
-                <tr>
-                    <td>Return on Assets</td>
-                    <td>${data['returnOnAssets']*100}%</td>
-                </tr>
-                <tr>
-                    <td>Return on Equity</td>
-                    <td>${data['returnOnEquity']*100}%</td>
-                </tr>
-                <tr>
-                    <td>Revenue Growth</td>
-                    <td>${data['revenueGrowth']*100}%</td>
-                </tr>
-                <tr>
-                    <td>Profit Margins</td>
-                    <td>${data['profitMargins']*100}%</td>
-                </tr>
+            <tr>
+            <th>Indicator</th>
+            <th>Value</th>
+            </tr>
+            <tr>
+            <td>Beta</td>
+            <td>${data['beta'].toFixed(2)}</td>
+            </tr>
+            <tr>
+            <td>Debt to Equity</td>
+            <td>${data['debtToEquity'].toFixed(2)}%</td>
+            </tr>
+            <tr>
+            <td>Return on Assets</td>
+            <td>${(data['returnOnAssets']*100).toFixed(2)}%</td>
+            </tr>
+            <tr>
+            <td>Return on Equity</td>
+            <td>${(data['returnOnEquity']*100).toFixed(2)}%</td>
+            </tr>
+            <tr>
+            <td>Revenue Growth</td>
+            <td>${(data['revenueGrowth']*100).toFixed(2)}%</td>
+            </tr>
+            <tr>
+            <td>Profit Margins</td>
+            <td>${(data['profitMargins']*100).toFixed(2)}%</td>
+            </tr>
             </table>
+            <br>
+            <a href="${data['website']}" target="_blank">Company Website</a>
         `;
     }
     else {
